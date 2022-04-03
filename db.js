8bc0a7a2c4ad0d22ea3bf39f73f1ecc1ad6146c7
ThanhBk <thanhbk11a1@gmail.com>
2022-04-03 20:33:10 +0700

    first commit

--- db.js
+++
@@ -1,9 +0,0 @@
-const items = require("./data/db");
-
-module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
-}
-
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const data = require("./db.js");
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
 
+const port = process.env.PORT || 5000;
 server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+server.use(router);
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
